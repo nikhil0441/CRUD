2fb721f9adc7a1c9c91ae3ffbae32bba8dc91257
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-01-18 23:04:11 +0530

    added

--- crud.js
+++ crud.js
@@ -1,25 +1,35 @@
 let fetchData = async () => {
   let url =`http://localhost:3000/Booking`;
-  try {
+
     let res = await fetch(url, { method: "GET" });
-    if (!res.ok) throw new Error("Failed to fetch data");
+    
     let data = await res.json();
     console.log(data);
 
     let show = document.querySelector("#showData");
     data.map((e) => {
-      show.innerHTML += `
-                <div style="border: 1px solid black; padding: 10px; margin: 10px;">
-                    <h2>Name: ${e.Name}</h2>
-                    <p>Age: ${e.Age}</p>
-                    <p>Checking In: ${e["Cheaking In"]}</p>
-                    <p>Check Out: ${e["Cheak Out"]}</p>
-                    <p>Phone Number: ${e["Phone Number"]}</p>
-                    <p>Members: ${e.Member}</p>
-                </div>
-            `;
+      // show.innerHTML += `
+      //           <div style="border: 1px solid black; padding: 10px; margin: 10px;">
+      //               <h2>Name: ${e.Name}</h2>
+      //               <p>Age: ${e.Age}</p>
+      //               <p>Checking In: ${e["Cheaking In"]}</p>
+      //               <p>Check Out: ${e["Cheak Out"]}</p>
+      //               <p>Phone Number: ${e["Phone Number"]}</p>
+      //               <p>Members: ${e.Member}</p>
+      //           </div>
+      //       `;
+      data.map((e)=>{
+        show.innerHTML += `
+        <tr>
+        <td>${e.name}</td>
+        <td>${e.Age}</td>
+        <td>${e.Cheaking_In}</td>
+        <td>${e.Cheak_Out}</td>
+        <td>${e.Phone_Number}</td>
+        <td>${e.Member}</td>
+        </tr>
+        `;
+      })
     });
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
-};
+  } 
+
